2e5efba916c18638a9fb12d9940ce59c8e509639
Onur <onurozet@hotmail.com>
2022-11-21 16:21:58 +0300

    all document added

---
+++ script.js
@@ -0,0 +1,7 @@
+var Ad_Soyad=prompt("Adinizi Giriniz");
+var Ogrno=prompt("Ogrenci Numaranizi Giriniz");
+var Ders=prompt("Brans Dersi");
+var not1=prompt("1.notunuzu giriniz");
+var not2=prompt("2.notunuzu giriniz");
+var sonuc=(Number(not1) + Number(not2))/2;
+console.log(Ogrno + " ogrenci numarali " + Ad_Soyad + " isimli ogrencinin " + Ders + " dersinden aldigi sonuc " + sonuc);
